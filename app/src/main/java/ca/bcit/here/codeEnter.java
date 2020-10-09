f7d487cf036a7f63fd6468cf9dbfd571098fbfca
Sean Wallace <swallace987@gmail.com>
2020-10-09 10:30:04 -0700

    Added a class liss activity that has boxes of classes.

---
+++ app/src/main/java/ca/bcit/here/codeEnter.java
@@ -0,0 +1,14 @@
+package ca.bcit.here;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class codeEnter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_code_enter);
+    }
+}
