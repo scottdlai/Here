c799d17694b81618d0be80f3f7061062120ced77
Sean Wallace <swallace987@gmail.com>
2020-10-09 09:53:49 -0700

    Added a class liss activity that has boxes of classes.

---
+++ app/src/main/java/ca/bcit/here/ClassListStudent.java
@@ -0,0 +1,14 @@
+package ca.bcit.here;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClassListStudent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_list_student);
+    }
+}
