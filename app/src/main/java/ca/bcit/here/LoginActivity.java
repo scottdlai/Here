0dddfaff3f471af153d7c0d693f494cb6285130a
Scott Lai <tuonglai3602@gmail.com>
2020-11-24 15:46:25 -0800

    Added signup functionality.

--- app/src/main/java/ca/bcit/here/LoginActivity.java
+++ app/src/main/java/ca/bcit/here/LoginActivity.java
@@ -76,7 +76,7 @@ public class LoginActivity extends AppCompatActivity {
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
             }
         });
     }
--- app/src/main/java/ca/bcit/here/SignUpActivity.java
+++ app/src/main/java/ca/bcit/here/SignUpActivity.java
@@ -1,19 +1,72 @@
 package ca.bcit.here;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
+    EditText nameField;
+
+    EditText emailField;
+
+    EditText passwordField;
+
+    Button signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        mAuth = FirebaseAuth.getInstance();
+
+        nameField = findViewById(R.id.nameField);
+
+        emailField = findViewById(R.id.emailField);
+
+        passwordField = findViewById(R.id.passwordField);
+
+        signUpBtn = findViewById(R.id.signUpBtn);
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp();
+            }
+        });
+    }
+
+    private void signUp() {
+        String name = nameField.getText().toString().trim();
+
+        String email = emailField.getText().toString().trim();
+
+        String password = passwordField.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    startActivity(new Intent(SignUpActivity.this, MainActivity.class));
+                } else {
+                    Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
